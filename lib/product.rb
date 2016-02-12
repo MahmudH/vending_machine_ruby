4a5e1372e601f9d6a5ce4d89c0a8499d01f514e3
mahmudh <mahmudhssn246@gmail.com>
2016-02-12 08:22:24 +0000

    updated readme

--- lib/product.rb
+++ lib/product.rb
@@ -6,10 +6,4 @@ class Product
     @items = {coke: [200, 5], fanta: [100, 3], latte: [50, 6]}
 
   end
-
-  def add_product(name, price, qty)
-    details = []
-    details.push price, qty
-    items[name] = details
-  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -25,16 +25,6 @@ describe Order do
     it 'should raise error if the money you gave is too little' do
       expect{ order.purchase(:coke, 1) }.to raise_error "Not enough money, please try again"
     end
-
-    # it 'provide correct change if the amount given is more than item price' do
-    #   order.purchase(:coke, 3)
-    #   expect(order.products[:coke][1]).to eq 1
-    # end
-
-
-    # it 'buy a product by giving name and correct amount of money' do
-    #   expect(order.purchase(:coke, 2)).to eq "Thank you for your purchase"
-    # end
   end
 
 end
